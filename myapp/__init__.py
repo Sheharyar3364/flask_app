ca802fa066235ead23361b6781eeb94785cb1760
Muhammad Sheharyar <sheharyar3364@gmail.com>
2023-11-16 13:06:20 +0500

    Added files

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -6,10 +6,11 @@ from .routes import main
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
     db.init_app(app)
 
     app.register_blueprint(main)
 
     return app
+
--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+# run.py
+
 from myapp import create_app
+from myapp.extensions import db
 
 app = create_app()
+
+if __name__ == '__main__':
+    # Create tables and run the app
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+#postgresql://strube_database_user:7TDGbsJgseZGCkNkQeEl1np1jaD7BvPw@dpg-clag1bhm6hds739prk10-a.oregon-postgres.render.com/strube_database

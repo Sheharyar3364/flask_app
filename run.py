0551fe47922ce0929a60b0f230dfb0fbd0cf6e10
Muhammad Sheharyar <96918155+Sheharyar3364@users.noreply.github.com>
2023-11-16 14:47:39 +0500

    Update run.py

--- run.py
+++ run.py
@@ -10,4 +10,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-# postgres://strube_database_user:7TDGbsJgseZGCkNkQeEl1np1jaD7BvPw@dpg-clag1bhm6hds739prk10-a/strube_database
